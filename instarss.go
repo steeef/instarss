da43113e7439f94617cbbcbe32f9e64e598abf03
Stephen Price <sprice@monsooncommerce.com>
2014-09-26 13:54:46 -0700

    initial test

---
+++ instarss.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"log"
+)
+
+const REDISHOST = "redis:6379"
+
+var c, err = redis.Dial("tcp", REDISHOST)
+
+func sendData(input string) string {
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.Do("SET", "test-string", input)
+	r, err := redis.String(c.Do("GET", "test-string"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return r
+}
+
+func main() {
+	r = sendData("test")
+	fmt.Println(r)
+	defer c.Close()
+}
